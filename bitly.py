0c68260efb1251394ed3b862d2d365627b0059a2
IrrevocableCascade <benny.eggert@gmail.com>
2014-02-22 10:06:42 +0100

    First

---
+++ bitly.py
@@ -0,0 +1,16 @@
+__author__ = 'Benjamin'
+
+from urllib import request
+import json
+
+token = 'd2a24187877cdc2cbf070a2e01757c30343b31ab'
+api_adress = 'https://api-ssl.bitly.com'
+long_url = input('Enter URL to shorten: \n')
+get = '/v3/shorten?access_token='+token+'&longUrl='+long_url
+url = api_adress + get
+
+#  rq = request.Request(url)
+response = request.urlopen(url)
+data = json.loads(response.read().decode('utf-8'))
+print(data['status_code'])
+
