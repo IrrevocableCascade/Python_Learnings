e20e1c2826086113cf16545d418a97149357868b
IrrevocableCascade <benny.eggert@gmail.com>
2014-03-05 14:57:52 +0100

    Learned new things!

---
+++ socket_test.py
@@ -0,0 +1,12 @@
+""" Tutorialspoint code
+"""
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+s.connect((host, port))
+print(s.recv(1024).decode('utf-8'))
+s.close                     # Close the socket when done
