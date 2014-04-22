714ac65aac72583215e9df552191ec34026e6a45
IrrevocableCascade <benny.eggert@gmail.com>
2014-04-22 13:46:26 +0200

    Learned new things!

--- Project Euler/Largest Prime Factor.py
+++ Project Euler/Largest Prime Factor.py
@@ -18,20 +18,24 @@ def find_factors(number):
 	return factors
 
 
-def is_prime(number):
-	i = 1
-	factors = []
-	if number == 2:
-		return True
-	if number % 2 == 0 or number == 1:
+def is_prime(n):
+	'''check if integer n is a prime'''
+	# make sure n is a positive integer
+	n = abs(int(n))
+	# 0 and 1 are not primes
+	if n < 2:
 		return False
-	else:
-		while i < number:
-			if number % i == 0:
-				factors.append(i)
-				if len(factors) > 2:
-					return False
-			i += 1
+	# 2 is the only even prime number
+	if n == 2:
+		return True
+		# all other even numbers are not primes
+	if not n & 1:
+		return False
+	# range starts with 3 and only needs to go up the squareroot of n
+	# for all odd numbers
+	for x in range(3, int(n ** 0.5) + 1, 2):
+		if n % x == 0:
+			return False
 	return True
 
 
